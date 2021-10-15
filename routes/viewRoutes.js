9b81788120b2176a0b24316f6cd2bbf418343259
Gbenga <gbengaogunleye34@gmail.com>
2021-10-15 16:11:15 +0100

    create a well structured and indented block of codes in tours.pug

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -9,6 +9,6 @@ const viewsController = require('./../controllers/viewsController');
 
 router.get('/', viewsController.getOverview);
 
-router.get('/tours', viewsController.getTour);
+router.get('/tours/:slug', viewsController.getTour);
 
 module.exports = router;
